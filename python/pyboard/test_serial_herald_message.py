10a04d58abb334682ccd562bf1a4986b3dea0418
Luc Libralesso <libralesso.l@gmail.com>
2015-07-06 15:39:33 +0200

    add test cases for testing herald serialisation over bluetooth

---
+++ python/pyboard/test_serial_herald_message.py
@@ -0,0 +1,16 @@
+from automata import *
+from serial_herald_message import *
+from uuid import uuid4
+
+a = SerialAutomata()
+reader=MessageReader(a)
+mi = SerialHeraldMessage('a', 'b', 'c', 'd', 'e', reply_to='nope')
+mi.set_uid(str(uuid4()))
+print(mi)
+print(mi.to_automata_string())
+a.read(mi.to_automata_string())
+
+m2 = None
+while not m2:
+    m2 = reader.read()
+print(m2)
