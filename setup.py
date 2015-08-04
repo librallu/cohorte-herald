330650e7fe7c1a9aa0178812d08af332e927fe98
Luc Libralesso <libralesso.l@gmail.com>
2015-08-04 21:29:07 +0200

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Cohorte Micronode',
+      version='0.9',
+      description='Cohorte Micronode Repository',
+      url='https://github.com/librallu/cohorte-herald',
+      author='Luc Libralesso',
+      author_email='libralesso.l@gmail.com',
+      license='Apache License 2.0',
+      packages=[],
+      zip_safe=False)
